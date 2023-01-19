0b563bf200cba9d7a4525f97ce60f47d2fde24b6
Aneeshaik <115131511+Aneeshaik@users.noreply.github.com>
2023-01-19 09:32:48 +0530

    Initial commit

---
+++ newpro.js
@@ -0,0 +1,54 @@
+for(var i=0; i<document.querySelectorAll(".drum").length; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var buttonInnerHtml = this.innerHTML;
+    makeSound(buttonInnerHtml);
+    buttonPressed(buttonInnerHtml);
+  });
+}
+document.addEventListener("keydown", function(event){
+  var keySound = event.key;
+  makeSound(keySound);
+  buttonPressed(keySound);
+});
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+        var tom2 = new Audio("tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        var tom3 = new Audio("tom-3.mp3");
+        tom3.play();
+        break;a
+      case "d":
+        var tom4 = new Audio('tom-4.mp3');
+        tom4.play();
+        break;
+      case "j":
+        var snare = new Audio("snare.mp3");
+        snare.play();
+        break;
+      case "k":
+        var crash = new Audio("crash.mp3");
+        crash.play();
+        break;
+      case "l":
+        var kick = new Audio("kick-bass.mp3");
+        kick.play();
+        break;
+      default:
+        console.log(buttonInnerHTML);
+        break;
+  }
+}
+function buttonPressed(currentKey){
+  var activeKey = document.querySelector("." + currentKey);
+  activeKey.classList.add("pressed");
+  setTimeout(function(){
+    activeKey.classList.remove("pressed")
+  }, 100)
+}
